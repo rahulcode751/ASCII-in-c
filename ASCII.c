15cc86f1b1379acbe8ebb997f65b224adebc1958
rahulcode751 <73958355+rahulcode751@users.noreply.github.com>
2020-12-27 14:41:11 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() {
+	char a;
+	scanf("%c",&a);
+	printf("the input value of character is %c", a);
+	ptintf("\n the ASCII value for the given input character is %d", a);
+	
+	
+}
